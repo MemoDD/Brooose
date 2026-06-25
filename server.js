763854993dbc64ff962b8a6882232c1d33ca697e
Memo DevIn <dmemo1663@gmail.com>
2026-06-26 01:40:16 +0530

    Update server.js
    Changed port to pick env vars

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ app.get("/products", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("My API running at http://localhost:3000");
 });
